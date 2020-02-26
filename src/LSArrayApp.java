e22d389aa328163110b6119b0bd4ac1a1e6a8f2d
taboka <nydtab001@myuct.ac.za>
2020-02-26 16:36:11 +0200

    edited

--- src/LSArrayApp.java
+++ src/LSArrayApp.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 public class LSArrayApp {
     public static void main(String[] args)throws Exception{
-        File file = new File("C:\Users\Taboka\Desktop\Course\CSC2001F\Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
+        File file;
+        file = new File("C:\\Users\\Taboka\\Desktop\\Course\\CSC2001F\\Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
         String stt;
